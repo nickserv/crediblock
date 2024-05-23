b4a064efc9df03446d169cc2e3696ddc44349d3e
Nick McCurdy <nick@nickmccurdy.com>
2024-05-23 06:28:07 -0400

    Tweak error handling

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,15 @@
 import { open } from "fs/promises";
 
 export async function GET() {
-	const agents = await Array.fromAsync((await open("agents.txt")).readLines());
-	const agentsString = agents.map((agent) => `User-agent: ${agent}`).join("\n");
-	return new Response(`${agentsString}\nDisallow: /`);
+	try {
+		const agents = await Array.fromAsync(
+			(await open("agents.txt")).readLines(),
+		);
+		const agentsString = agents
+			.map((agent) => `User-agent: ${agent}`)
+			.join("\n");
+		return new Response(`${agentsString}\nDisallow: /`);
+	} catch (error) {
+		console.error(error);
+	}
 }
