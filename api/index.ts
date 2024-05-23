2cb171355876a275deb383ee72985cafb1979e1a
Nick McCurdy <nick@nickmccurdy.com>
2024-05-23 07:04:38 -0400

    Revert "Tweak error handling"
    This reverts commit b4a064efc9df03446d169cc2e3696ddc44349d3e.

--- api/index.ts
+++ api/index.ts
@@ -2,15 +2,9 @@ import { open } from "fs/promises";
 import { resolve } from "path";
 
 export async function GET() {
-	try {
-		const agents = await Array.fromAsync(
-			(await open(resolve(import.meta.dirname, "../agents.txt"))).readLines(),
-		);
-		const agentsString = agents
-			.map((agent) => `User-agent: ${agent}`)
-			.join("\n");
-		return new Response(`${agentsString}\nDisallow: /`);
-	} catch (error) {
-		console.error(error);
-	}
+	const agents = await Array.fromAsync(
+		(await open(resolve(import.meta.dirname, "../agents.txt"))).readLines(),
+	);
+	const agentsString = agents.map((agent) => `User-agent: ${agent}`).join("\n");
+	return new Response(`${agentsString}\nDisallow: /`);
 }
